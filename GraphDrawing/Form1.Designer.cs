897380ad5ac117eb65732677d64bfb4a404ea83e
Dave Viggiano <dave.viggiano@gmail.com>
2019-10-26 12:04:07 -0400

    Initial Commit.

---
+++ GraphDrawing/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace GraphDrawing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.graphImage = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.windowInput = new System.Windows.Forms.NumericUpDown();
+            this.fillButton = new System.Windows.Forms.Button();
+            this.colorSchemePicker = new System.Windows.Forms.ComboBox();
+            this.gridLinesInput = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.selectionImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.graphImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.windowInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridLinesInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.selectionImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // graphImage
+            // 
+            this.graphImage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.graphImage.Location = new System.Drawing.Point(12, 42);
+            this.graphImage.Name = "graphImage";
+            this.graphImage.Size = new System.Drawing.Size(727, 372);
+            this.graphImage.TabIndex = 0;
+            this.graphImage.TabStop = false;
+            this.graphImage.MouseDown += new System.Windows.Forms.MouseEventHandler(this.graphImage_MouseDown);
+            this.graphImage.MouseMove += new System.Windows.Forms.MouseEventHandler(this.graphImage_MouseMove);
+            this.graphImage.MouseUp += new System.Windows.Forms.MouseEventHandler(this.graphImage_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Smooth";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // windowInput
+            // 
+            this.windowInput.Location = new System.Drawing.Point(95, 14);
+            this.windowInput.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.windowInput.Name = "windowInput";
+            this.windowInput.Size = new System.Drawing.Size(49, 20);
+            this.windowInput.TabIndex = 2;
+            this.windowInput.Value = new decimal(new int[] {
+            101,
+            0,
+            0,
+            0});
+            // 
+            // fillButton
+            // 
+            this.fillButton.Location = new System.Drawing.Point(198, 13);
+            this.fillButton.Name = "fillButton";
+            this.fillButton.Size = new System.Drawing.Size(75, 23);
+            this.fillButton.TabIndex = 3;
+            this.fillButton.Text = "Fill";
+            this.fillButton.UseVisualStyleBackColor = true;
+            this.fillButton.Click += new System.EventHandler(this.fillButton_Click);
+            // 
+            // colorSchemePicker
+            // 
+            this.colorSchemePicker.FormattingEnabled = true;
+            this.colorSchemePicker.Items.AddRange(new object[] {
+            "Light",
+            "Dark"});
+            this.colorSchemePicker.Location = new System.Drawing.Point(672, 15);
+            this.colorSchemePicker.Name = "colorSchemePicker";
+            this.colorSchemePicker.Size = new System.Drawing.Size(67, 21);
+            this.colorSchemePicker.TabIndex = 4;
+            this.colorSchemePicker.Text = "Light";
+            this.colorSchemePicker.SelectedIndexChanged += new System.EventHandler(this.colorSchemePicker_SelectedIndexChanged);
+            // 
+            // gridLinesInput
+            // 
+            this.gridLinesInput.Location = new System.Drawing.Point(348, 13);
+            this.gridLinesInput.Name = "gridLinesInput";
+            this.gridLinesInput.Size = new System.Drawing.Size(59, 20);
+            this.gridLinesInput.TabIndex = 5;
+            this.gridLinesInput.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.gridLinesInput.ValueChanged += new System.EventHandler(this.gridLinesInput_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(414, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Grid Lines";
+            // 
+            // selectionImage
+            // 
+            this.selectionImage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.selectionImage.Location = new System.Drawing.Point(13, 420);
+            this.selectionImage.Name = "selectionImage";
+            this.selectionImage.Size = new System.Drawing.Size(726, 30);
+            this.selectionImage.TabIndex = 7;
+            this.selectionImage.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 459);
+            this.Controls.Add(this.selectionImage);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gridLinesInput);
+            this.Controls.Add(this.colorSchemePicker);
+            this.Controls.Add(this.fillButton);
+            this.Controls.Add(this.windowInput);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.graphImage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.graphImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.windowInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridLinesInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.selectionImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox graphImage;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown windowInput;
+        private System.Windows.Forms.Button fillButton;
+        private System.Windows.Forms.ComboBox colorSchemePicker;
+        private System.Windows.Forms.NumericUpDown gridLinesInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox selectionImage;
+    }
+}
+
---
+++ GraphDrawing/Form1.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GraphDrawing
+{
+    /*
+     * TODO:
+     * -Control to highlight a window, then either delete or smooth within window
+     * -Save image
+     * 
+     * Presentation TODOs:
+     * -Title, axes, axis titles/labels
+     * -Annotations
+     * -
+     */
+    public enum ColorScheme
+    {
+        Light = 1,
+        Dark = 2
+    }
+
+    public partial class Form1 : Form
+    {
+        private const int DataSize = 1000;
+        private double[] mData = new double[DataSize];
+
+        private ColorScheme mScheme = ColorScheme.Light;
+        private Bitmap mImage = null;
+        private Bitmap mSelectionImage = null;
+
+        private bool mTracking = false;
+        private Point mLastPosition = new Point();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < DataSize; i++)
+            {
+                mData[i] = -1;
+            }
+
+            DrawFromScratch();
+        }
+
+        private Pen GetPen(Color color)
+        {
+            Pen pen = new Pen(color);
+
+            pen.Width = 1;
+
+            return pen;
+        }
+
+        private void DrawFromScratch()
+        {
+            mSelectionImage = new Bitmap(selectionImage.Width, selectionImage.Height);
+            Graphics g = Graphics.FromImage(mSelectionImage);
+            Brush brush = new SolidBrush(Color.White);
+            g.FillRectangle(brush, 0, 0, selectionImage.Width, selectionImage.Height);
+            g.DrawRectangle(new Pen(Color.Black), 0, 0, selectionImage.Width-1, selectionImage.Height-1);
+            selectionImage.Image = mSelectionImage;
+
+            mImage = new Bitmap(graphImage.Width, graphImage.Height);
+            //Console.WriteLine("{0}, {1}", graphImage.Width, graphImage.Height);
+
+            g = Graphics.FromImage(mImage);
+            brush = new SolidBrush(mScheme == ColorScheme.Light ? Color.White : Color.Black);
+            g.FillRectangle(brush, 0, 0, graphImage.Width, graphImage.Height);
+
+            DrawAxes();
+
+            for (int x = 0; x < DataSize - 1; x++)
+            {
+                updateGraph(x, mData[x], x + 1, mData[x + 1]);
+            }
+
+            graphImage.Image = mImage;
+        }
+
+        private void DrawAxes()
+        {
+            Graphics g = Graphics.FromImage(mImage);
+
+            Pen pen = new Pen(Color.Gray);
+            int numAxes = (int)gridLinesInput.Value;
+            for (int offset = 1; offset < numAxes+1; offset++)
+            {
+                int pixel = (int)Math.Round((double)offset / (numAxes+1) * graphImage.Height);
+                g.DrawLine(pen, 0, pixel, graphImage.Width, pixel);
+                pixel = (int)Math.Round((double)offset / (numAxes+1) * graphImage.Width);
+                g.DrawLine(pen, pixel, 0, pixel, graphImage.Height);
+            }
+        }
+
+        private void graphImage_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mTracking)
+            {
+                Point position = getMousePosition();
+
+                if (position.X >= 0 && position.X < graphImage.Width &&
+                    position.Y >= 0 && position.Y < graphImage.Height)
+                {
+                    int lastX = (int)Math.Round((double)mLastPosition.X * DataSize / graphImage.Width);
+                    double lastY = (double)(graphImage.Height - mLastPosition.Y) * DataSize / graphImage.Height;
+
+                    int x = (int)Math.Round((double)position.X * DataSize / graphImage.Width);
+                    double y = (double)(graphImage.Height - position.Y) / graphImage.Height * DataSize;
+
+                    updateGraph(lastX, lastY, x, y);
+
+                    //Console.WriteLine("{0}, {1}", x, y);
+
+                    mLastPosition = position;
+                }
+            }
+        }
+
+        private void ChangePoint(int x, double newY)
+        {
+            int imageX = (int)Math.Round((double)x / DataSize * graphImage.Width);
+            Pen bgPen = GetPen(mScheme == ColorScheme.Light ? Color.White : Color.Black);
+            Pen linePen = GetPen(mScheme == ColorScheme.Light ? Color.Black : Color.White);
+            Graphics g = Graphics.FromImage(mImage);
+
+            int imageYOld = graphImage.Height - (int)Math.Round(mData[x] / DataSize * graphImage.Height);
+            if (x > 0 && mData[x - 1] >= 0)
+            {
+                int prevImageX = (int)Math.Round((double)(x - 1) / DataSize * graphImage.Width);
+                int prevImageY = graphImage.Height - (int)Math.Round(mData[x - 1] / DataSize * graphImage.Height);
+                g.DrawLine(bgPen, prevImageX, prevImageY, imageX, imageYOld);
+            }
+            if (x < DataSize - 1 && mData[x + 1] >= 0)
+            {
+                int nextImageX = (int)Math.Round((double)(x + 1) / DataSize * graphImage.Width);
+                int nextImageY = graphImage.Height - (int)Math.Round(mData[x + 1] / DataSize * graphImage.Height);
+                g.DrawLine(bgPen, nextImageX, nextImageY, imageX, imageYOld);
+            }
+
+            if (newY >= 0)
+            {
+                int imageYNew = graphImage.Height - (int)Math.Round(newY / DataSize * graphImage.Height);
+                if (x > 0 && mData[x - 1] >= 0)
+                {
+                    int prevImageX = (int)Math.Round((double)(x - 1) / DataSize * graphImage.Width);
+                    int prevImageY = graphImage.Height - (int)Math.Round(mData[x - 1] / DataSize * graphImage.Height);
+                    g.DrawLine(linePen, prevImageX, prevImageY, imageX, imageYNew);
+                }
+                if (x < DataSize - 1 && mData[x + 1] >= 0)
+                {
+                    int nextImageX = (int)Math.Round((double)(x + 1) / DataSize * graphImage.Width);
+                    int nextImageY = graphImage.Height - (int)Math.Round(mData[x + 1] / DataSize * graphImage.Height);
+                    g.DrawLine(linePen, nextImageX, nextImageY, imageX, imageYNew);
+                }
+            }
+
+            mData[x] = newY;
+        }
+
+        private void updateGraph(int lastX, double lastY, int x, double y)
+        {
+            int minX = Math.Min(lastX, x);
+            double minY = minX == lastX ? lastY : y;
+            int maxX = Math.Max(lastX, x);
+            double maxY = maxX == lastX ? lastY : y;
+
+            for (int curX = minX; curX <= maxX; curX++)
+            {
+                double yInterp = y;
+                if (minX != maxX)
+                {
+                    yInterp = ((double)curX - minX) / (maxX - minX) * (maxY - minY) + minY;
+                }
+
+                ChangePoint(curX, yInterp);
+            }
+
+            //DrawFromScratch();
+            DrawAxes();
+            graphImage.Image = mImage;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int window = (int)windowInput.Value;
+            double[] newData = new double[DataSize];
+            for (int x = 0; x < DataSize; x++)
+            {
+                if (mData[x] < 0)
+                {
+                    newData[x] = mData[x];
+                }
+                else
+                {
+                    double total = 0;
+                    int count = 0;
+                    for (int subX = x - window / 2; subX <= x + window / 2; subX++)
+                    {
+                        if (subX >= 0 && subX < DataSize && mData[subX] >= 0)
+                        {
+                            total += mData[subX];
+                            count++;
+                        }
+                    }
+
+                    newData[x] = total / count;
+                }
+            }
+
+            mData = newData;
+
+            int empty = 0;
+            int firstNonEmpty = -1;
+            for (int x = 0; x < DataSize; x++)
+            {
+                if (mData[x] < 0)
+                {
+                    empty++;
+                }
+                else if (firstNonEmpty < 0)
+                {
+                    firstNonEmpty = x;
+                }
+            }
+            Console.WriteLine("{0} empty ({1} first non-empty)", empty, firstNonEmpty);
+
+            DrawFromScratch();
+        }
+
+        private void fillButton_Click(object sender, EventArgs e)
+        {
+            //Find the first valid sample
+            int firstFilledIndex = -1;
+            for (int i = 0; i < DataSize; i++)
+            {
+                if (mData[i] >= 0)
+                {
+                    firstFilledIndex = i;
+                    break;
+                }
+            }
+
+            //Only proceed if at least one sample is filled
+            if (firstFilledIndex >= 0)
+            {
+                //Fill from beginning to firstFilledSample
+                for (int i = 0; i < firstFilledIndex; i++)
+                {
+                    mData[i] = mData[firstFilledIndex];
+                }
+
+                //Interpolate gaps
+                int lastFullSample = firstFilledIndex;
+                for (int i = firstFilledIndex+1; i < DataSize; i++)
+                {
+                    if (mData[i] >= 0)
+                    {
+                        if (lastFullSample != i - 1)
+                        {
+                            //Fill a gap
+                            for (int j = lastFullSample + 1; j < i; j++)
+                            {
+                                mData[j] = mData[lastFullSample] + ((double)j - lastFullSample) / (i - lastFullSample) * (mData[i] - mData[lastFullSample]);
+                            }
+                        }
+
+                        lastFullSample = i;
+                    }
+                }
+
+                //Fill to end
+                for (int i = lastFullSample + 1; i < DataSize; i++)
+                {
+                    mData[i] = mData[lastFullSample];
+                }
+
+                DrawFromScratch();
+            }
+        }
+
+        private Point getMousePosition()
+        {
+            Point position = graphImage.PointToClient(System.Windows.Forms.Cursor.Position);
+
+            return position;
+        }
+
+        private void graphImage_MouseDown(object sender, MouseEventArgs e)
+        {
+            mTracking = true;
+            mLastPosition = getMousePosition();
+        }
+
+        private void graphImage_MouseUp(object sender, MouseEventArgs e)
+        {
+            mTracking = false;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            DrawFromScratch();
+        }
+
+        private void colorSchemePicker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            mScheme = colorSchemePicker.Text.Equals("Light") ? ColorScheme.Light : ColorScheme.Dark;
+            DrawFromScratch();
+        }
+
+        private void gridLinesInput_ValueChanged(object sender, EventArgs e)
+        {
+            DrawFromScratch();
+        }
+    }
+}
